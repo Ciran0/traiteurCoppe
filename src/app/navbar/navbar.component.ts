f3cc2ce802fe38eb3bbb4242cd4f84c1dcbd55b7
Ciran0 <jonasgendebien@proton.me>
2024-08-27 22:51:45 +0200

    working navbar frontend

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ProfileCardComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  animations: [
+    trigger('openClose', [
+      // ...
+      state(
+        'open',
+        style({
+         left: '0vw'
+        }),
+      ),
+      state(
+        'closed',
+        style({
+          left: '-150vw'
+        })
+      ),
+      transition('open => closed', [animate('0.5s ease-in-out')]),
+      transition('closed => open', [animate('0.5s ease-in-out')])
+    ]),
+  ]
 })
-export class NavbarComponent {
+export class NavbarComponent{
 
+    isOpen = false;
+  toggle() {
+    this.isOpen = !this.isOpen;
+    console.log("hello world");
+  }
 }
