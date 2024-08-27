5f3bd416449a6e778c9c121bddfdb408f4560eea
Ciran0 <jonasgendebien@proton.me>
2024-08-27 23:16:59 +0200

    home component
    creation of the home component to prepare for the routing

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ArticleComponent } from './article/article.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ArticleComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
