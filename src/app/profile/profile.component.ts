2cefb8cf65e3b8ffbb6c76a1e0f9afeb7e41e763
Ciran0 <jonasgendebien@proton.me>
2024-08-28 13:02:42 +0200

    barebone profile page
    html only profile page with forms to change info (profile pic will use
    gravatar)

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  nom: string = '';
+  email: string = '';
+  oldpswd: string = '';
+  newpswd: string = '';
+  newpswdconf: string = '';
 
+
+  onSubmit() {
+    console.log(`Nom: ${this.nom}`);
+    console.log(`Email: ${this.email}`);
+    //Ici on peut envoyer les données à un service ou à une API
+  }
+  modUsername(){}
+  modEmail(){}
+  modPswd(){}
 }
